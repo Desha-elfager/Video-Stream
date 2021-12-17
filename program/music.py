a46c02da03a96c4e584042bf8f9fbea867e00b03
Desha_Elfager <96248074+Desha-elfager@users.noreply.github.com>
2021-12-17 23:20:21 +0200

    Update music.py

--- program/music.py
+++ program/music.py
@@ -258,4 +258,4 @@ async def play(c: Client, m: Message):
                         except Exception as ep:
                             await suhu.delete()
                             await m.reply_text(f"🚫 error: `{ep}`")
-🚫 error: `{ep}`")
+
